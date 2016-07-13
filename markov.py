b9b6e0b78961d7736d115213065c38dce7a36888
Hackbright Student <info@hackbrightacademy.com>
2016-07-12 17:11:27 -0700

    Take text file from command line.

--- markov.py
+++ markov.py
@@ -1,4 +1,5 @@
 from random import choice
+import sys
 
 
 def open_and_read_file(file_path):
@@ -87,7 +88,7 @@ def make_text(chains):
     return text
 
 
-input_path = "green-eggs.txt"
+input_path = sys.argv[1]
 
 # Open the file and turn it into one long string
 input_text = open_and_read_file(input_path)
